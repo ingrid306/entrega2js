c9119ca863dc7ec944a5179ab97ee514dc42cf0a
ingrid306 <inpaito87@gmail.com>
2024-10-23 23:07:22 -0300

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+//a)  Las pizzas que tengan un id impar.
+
+//b) Responder: ¿Hay alguna pizza que valga menos de $600?
+
+//c) El nombre de cada pizza con su respectivo precio.
+
+
+//d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). 
