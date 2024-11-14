33d6e3d47ce6425fe2d50c108936a6aa84019e14
ingrid306 <inpaito87@gmail.com>
2024-11-13 21:35:38 -0300

    .

--- index.js
+++ index.js
@@ -47,3 +47,49 @@ const pizzas = [
     ingredientes: ["Muzzarella", "Tomate", "Anana"],
   },
 ];
+
+
+//a)  Las pizzas que tengan un id impar.
+
+const pizzasImpares = pizzas.filter ((pizza)=> pizza.id % 2 !== 0) 
+pizzasImpares.forEach ((pizza)=> 
+console.log('La ${pizza.nombre} tiene un id impar'));
+
+
+//b) Responder: ¿Hay alguna pizza que valga menos de $600?
+
+const precioMenorA600 (pizza) => pizza precio < 600;
+
+if (pizzas.some(precioMenonA600)) {
+
+console.log("Hay pizzas con precio menor a $600");
+
+} else {
+
+console.log("No hay pizzas con precio menor a $600");
+
+}
+
+const pizzaBarata = pizzas.find(precioMenorA600);
+
+if (!pizzaBarata) {
+  
+  console.log("No hay pizzas con precio menor a $600");
+
+} else { 
+  console.log("Hay pizzas con precio menor a $600");
+}
+
+//c) El nombre de cada pizza con su respectivo precio.
+
+pizzas.forEach((pizza) =>
+  console.log (
+    `Vení a probar nuestra ${pizza.nombre} por tan solo $${pizza.precio´}
+));
+
+//d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). 
+
+pizzas.forEach((pizza) => {
+  console.log (`Los ingredientes de la ${pizza.nombre} son:´);
+pizza.ingredientes.forEach((ingrediente)=> console.log(ingrediente));
+});
